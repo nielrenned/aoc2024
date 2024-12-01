e7fc446c7b3880a5522f68e6c07e687545f904f4
Jacob Dennnerlein <nielrenned@gmail.com>
2024-12-01 12:47:42 -0500

    Update template

--- scripts/newday.py
+++ scripts/newday.py
@@ -6,6 +6,7 @@ template = """DAY = {day}
 RAW_INPUT = None
 INPUT = None
 
+
 def load_input(use_test_input=False):
     global RAW_INPUT
     path = f'inputs/actual/day{{DAY:02}}.txt'
@@ -14,6 +15,7 @@ def load_input(use_test_input=False):
     with open(path) as f:
         RAW_INPUT = f.read()
 
+
 def parse_input():
     global INPUT
     INPUT = []
@@ -21,17 +23,21 @@ def parse_input():
         if line == '': continue
         INPUT.append(line)
 
+
 def part1():
     pass
 
+
 def part2():
     pass
+
 
 def main():
     load_input(use_test_input=False)
     parse_input()
     print('PART 1:', part1())
     # print('PART 2:', part2())
+
 
 if __name__ == "__main__":
     main()
