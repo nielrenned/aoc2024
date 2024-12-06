16c3fffc452bb91012adc9f238b1f728bd74a6e6
Jacob Dennerlein <nielrenned@gmail.com>
2024-12-06 11:34:00 -0500

    Day 6 Finished

---
+++ solutions/day06.py
@@ -0,0 +1,87 @@
+from itertools import product
+from tqdm import tqdm
+
+DAY = 6
+RAW_INPUT = None
+INPUT = None
+
+
+def load_input(use_test_input=False):
+    global RAW_INPUT
+    path = f'inputs/actual/day{DAY:02}.txt'
+    if use_test_input:
+        path = f'inputs/test/day{DAY:02}.txt'
+    with open(path) as f:
+        RAW_INPUT = f.read()
+
+
+def parse_input():
+    global INPUT
+    INPUT = []
+    obstacles = set()
+    guard = None
+    w = 0
+    h = 0
+
+    for y, line in enumerate(RAW_INPUT.split('\n')):
+        if line == '': continue
+        h = max(h, y+1)
+        for x, c in enumerate(line):
+            if c == '#': obstacles.add((x, y))
+            if c == '^': guard = (x, y)
+            w = max(w, x+1)
+    
+    INPUT = w, h, guard, obstacles
+
+
+DIRECTIONS = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+
+
+def guard_step(direction, position, obstacles):
+    dx, dy = direction
+    gx, gy = position
+    next_position = (gx + dx, gy + dy)
+    if next_position in obstacles:
+        direction_index = DIRECTIONS.index(direction)
+        new_direction = DIRECTIONS[(direction_index + 1) % 4]
+        return guard_step(new_direction, position, obstacles)
+    else:
+        return direction, next_position
+
+
+def part1():
+    w, h, (gx, gy), obstacles = INPUT
+    direction = DIRECTIONS[0]
+    locations = set()
+    while 0 <= gx < w and 0 <= gy < h:
+        locations.add((gx, gy))
+        direction, (gx, gy) = guard_step(direction, (gx, gy), obstacles)
+    return len(locations)
+
+
+def part2():
+    w, h, (start_x, start_y), start_obstacles = INPUT
+    count = 0
+    for x, y in tqdm(list(product(range(w), range(h)))):
+        if (x, y) == (start_x, start_y): continue
+        obstacles = start_obstacles | {(x, y)}
+        direction = DIRECTIONS[0]
+        locations = set()
+        gx, gy = start_x, start_y
+        while 0 <= gx < w and 0 <= gy < h:
+            if (direction, gx, gy) in locations:
+                count += 1
+                break
+            locations.add((direction, gx, gy))
+            direction, (gx, gy) = guard_step(direction, (gx, gy), obstacles)
+    return count
+
+def main():
+    load_input(use_test_input=False)
+    parse_input()
+    print('PART 1:', part1())
+    print('PART 2:', part2())
+
+
+if __name__ == "__main__":
+    main()
