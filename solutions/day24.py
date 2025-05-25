bbca9176a33fa587111c042689d68e26ab6666ad
Jacob Dennerlein <nielrenned@gmail.com>
2025-05-25 16:17:20 -0400

    Day 24 Finished

--- solutions/day24.py
+++ solutions/day24.py
@@ -28,7 +28,7 @@ def parse_input():
     for line in lines[first_blank+1:]:
         if line == '': continue
         expression, output = line.split(' -> ')
-        connections[output] = expression.split(' ')
+        connections[output] = tuple(expression.split(' '))
     
     INPUT = inital_inputs, connections
 
@@ -39,8 +39,8 @@ def perform(a, op, b):
     if op == 'XOR': return a ^ b
 
 
-def part1():
-    values, connections = INPUT
+def simulate_to_steady_state(starting_values, connections):
+    values = {name: value for name, value in starting_values.items()}
 
     did_change = True
     while did_change:
@@ -50,20 +50,65 @@ def part1():
             values[output] = perform(values[a], op, values[b])
             did_change = True
     
+    return values
+
+
+def part1():
+    initial_values, connections = INPUT
+
+    values = simulate_to_steady_state(initial_values, connections)
+    
     z_outputs = sorted(filter(lambda s: s.startswith('z'), values.keys()))[::-1]
     z_bits = ''.join(str(values[z]) for z in z_outputs)
     return int(z_bits, 2)
 
 
+def find_output(a, op, b):
+    _, connections = INPUT
+    for output in connections:
+        if connections[output] == (a, op, b) or connections[output] == (b, op, a):
+            return output
+    return None
+
+
+def find_operations_on(wire):
+    _, connections = INPUT
+    operations = set()
+    for a, op, b in connections.values():
+        if a == wire or b == wire:
+            operations.add(op)
+    return operations
+
+
 def part2():
-    pass
+    given_values, connections = INPUT
+    precision = len(given_values)//2 + 1
+
+    last_output = f'z{precision-1:02}'
+    
+    wrong_outputs = set()
+    for output, (a, op, b) in connections.items():
+        xy_inputs = {a[0], b[0]} == {'x', 'y'}
+        ops_on_output = find_operations_on(output)
+
+        if op == 'AND' and ops_on_output != {'OR'} and {a, b} != {'x00', 'y00'}:
+            wrong_outputs.add(output)
+        elif op == 'OR' and ops_on_output != {'XOR', 'AND'} and output != last_output:
+            wrong_outputs.add(output)
+        elif op == 'XOR':
+            if not xy_inputs and ops_on_output != set():
+                wrong_outputs.add(output)
+            elif xy_inputs and ops_on_output != {'XOR', 'AND'} and output != 'z00':
+                wrong_outputs.add(output)
+
+    return ','.join(sorted(wrong_outputs))
 
 
 def main():
     load_input(use_test_input=False)
     parse_input()
     print('PART 1:', part1())
-    # print('PART 2:', part2())
+    print('PART 2:', part2())
 
 
 if __name__ == "__main__":
